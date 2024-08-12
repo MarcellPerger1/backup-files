0529fd822422aeec6c9abf37ab1ae6d8783b5180
Marcell Perger <marcell.perger.1@gmail.com>
2024-08-12 16:19:45 +0100

    Extract general utils -> `py_utils.py`

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import warnings
 from abc import ABC, abstractmethod
 from enum import StrEnum, IntEnum
 from pathlib import Path
-from typing import Sequence, Iterable, TypeVar, TypeGuard
+from typing import Sequence
 
-T = TypeVar('T')
+from py_util import flatten
 
 
 class FsType(StrEnum):
@@ -145,28 +145,6 @@ class Stats:
 
     def add_dir(self, _path: Path):
         self.n_dirs += 1
-
-
-# Note: this must be TypeGuard NOT TypeIs because
-#  true => iterable
-#  BUT false does NOT => non-iterable
-#   (because this returns False for str which is technically an iterable)
-def _is_actual_iterable(o: object) -> TypeGuard[Iterable]:
-    if isinstance(o, (str, bytes)):
-        # Special case - technically 'iterables' but not in a useful way for us here
-        return False
-    try:
-        iter(o)  # type: ignore
-    except (TypeError, NotImplementedError):
-        return False
-    return True
-
-
-# Note: `|` is NOT commutative (as it checks LHS first to see if it matches that)
-# So here, `T | Iterable` would mean that the type param of `ls`
-#  always gets bound to `T` (even if it is iterable) as it always matches `T`
-def flatten(ls: Iterable[Iterable[T] | T]) -> list[T]:
-    return [item for sub in ls for item in (sub if _is_actual_iterable(sub) else [sub])]
 
 
 class ListFiles:
---
+++ py_util.py
@@ -0,0 +1,29 @@
+"""General Python utils (not specific to this project)"""
+
+from __future__ import annotations
+
+from typing import TypeVar, TypeGuard, Iterable
+
+T = TypeVar('T')
+
+
+# Note: this must be TypeGuard NOT TypeIs because
+#  true => iterable
+#  BUT false does NOT => non-iterable
+#   (because this returns False for str which is technically an iterable)
+def _is_actual_iterable(o: object) -> TypeGuard[Iterable]:
+    if isinstance(o, (str, bytes)):
+        # Special case - technically 'iterables' but not in a useful way for us here
+        return False
+    try:
+        iter(o)  # type: ignore
+    except (TypeError, NotImplementedError):
+        return False
+    return True
+
+
+# Note: `|` is NOT commutative (as it checks LHS first to see if it matches that)
+# So here, `T | Iterable` would mean that the type param of `ls`
+#  always gets bound to `T` (even if it is iterable) as it always matches `T`
+def flatten(ls: Iterable[Iterable[T] | T]) -> list[T]:
+    return [item for sub in ls for item in (sub if _is_actual_iterable(sub) else [sub])]
